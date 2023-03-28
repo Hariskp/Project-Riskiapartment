0a0b2ea089b6d49eeb5008f8ed44b2b89d340168
Hariskp <haris22461238@gmail.com>
2023-03-28 17:14:19 +0700

    Import and Create window
    Import tkinter, sqlite3 and Build mainwindow function, All of this just for test

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import*
+import sqlite3
+from tkinter import ttk
+from tkinter import messagebox
+
+#Create Main Window
+def mainwindow() : 
+    root = Tk()
+    x = root.winfo_screenwidth()/2 - w/2
+    y = root.winfo_screenheight()/2 - h/2
+    root.geometry("%dx%d+%d+%d"%(w,h,x,y))
+    root.config(bg='#EBFCE0')
+    root.title("Riski Apartment : Login & Register")
+    root.option_add("*font", "Verdana 16 bold")
+    root.resizable(False, False)
+    root.rowconfigure((0,1,2,3), weight=1)
+    root.columnconfigure((0,1,2,3),weight=1)
+    return root
+
+#Program resolution
+w = 1300
+h = 700
+
+root = mainwindow()
+root.mainloop()
