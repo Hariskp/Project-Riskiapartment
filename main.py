d7d2893697d5a82088a00163a6c39d45b212853d
Hariskp <haris22461238@gmail.com>
2023-03-28 20:26:56 +0700

    Create login_fn()
    Create login_fn() and i got error about import picture

--- main.py
+++ main.py
@@ -10,16 +10,37 @@ def mainwindow() :
     y = root.winfo_screenheight()/2 - h/2
     root.geometry("%dx%d+%d+%d"%(w,h,x,y))
     root.config(bg='#EBFCE0')
-    root.title("Riski Apartment : Login & Register")
+    root.title("Riski Apartment : Login")
     root.option_add("*font", "Verdana 16 bold")
     root.resizable(False, False)
     root.rowconfigure((0,1,2,3), weight=1)
     root.columnconfigure((0,1,2,3),weight=1)
+    login_fn()
     return root
 
-#Program resolution
-w = 1300
-h = 700
+def createconnection() : #Create Connection to sqlite3 (สร้างรอไว้ก่อน)
+    global conn, cursor
+    conn = sqlite3.connect('input database right here')
+    cursor = conn.cursor()
 
+def login_fn() :
+    
+    login_frame = Frame(root, bg='red')
+    login_frame.place(x=0, y=0, width = w, height = h)
+    login_frame.rowconfigure((0,1,2,3), weight=1)
+    login_frame.columnconfigure((0,1,2,3), weight=1)
+    bg = Label(login_frame, image=bg_login)
+
+
+#Program resolution
+# w = 1300
+# h = 700
+w = 1500
+h = 800
+
+#Image import
+
+#Background
+bg_login = PhotoImage('file = img/img_login.png')
 root = mainwindow()
 root.mainloop()
