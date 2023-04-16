d8f3e209a834ad055c56e95489e70b1d84b6531d
chanutkid <chanut.kidw@gmail.com>
2023-04-17 00:52:13 +0700

    resize pdf

--- pdf_nut.py
+++ pdf_nut.py
@@ -1,51 +1,71 @@
 from fpdf import FPDF
+from PIL import Image
 
-# Define the receipt information
-customer_name = "John Smith"
-room_number = "101"
-start_date = "April 1, 2023"
-end_date = "April 7, 2023"
-price_per_night = 50
-total_price = price_per_night * 6
+renter_name = "Haris Kirdpakdee" #data base
+room_number = "101" #data base
+rent_amount = 1000
+deposit_amount = 500
+total_amount = rent_amount + deposit_amount
+date = "April 16, 2023" #data base
+address = "123 Pachalam Khuen Bog" #data base
+admin_name = "Jittawat Praditseree" #data base
+phone_number = "099-182-8589" #data base
+email = "jittawatofficial@gmail.com"
 
-# Create a new PDF object
 pdf = FPDF()
-
-# Add a page
 pdf.add_page()
 
-# Set the font and size for the title
-pdf.set_font("Arial", "B", 16)
+pdf.set_font("Arial", size=22, style="B")
 
-# Add the title
-pdf.cell(0, 10, "Renting Room Receipt", 0, 1, "C")
+pdf.cell(0, 15, "RISKI APARTMENT RECEIPT", 0, 1, "C")
+pdf.cell(0, 10, "", 0, 1)
+pdf.line(10, 28, 200, 28)
 
-# Set the font and size for the receipt information
-pdf.set_font("Arial", "", 12)
+pdf.image("img/img_riskilogo_black.png", x=10, y=1, w=30, h=30)
 
-# Add the customer name
-pdf.cell(50, 10, "Customer Name :", 0)
-pdf.cell(0, 10, customer_name, 0, 1)
+pdf.set_font("Arial", size=12, style="B")
 
-# Add the room number
+pdf.cell(50, 10, "Date :", 0 )
+pdf.cell(0, 10, date, 0, 1,"C")
+pdf.cell(50, 10, "Renter Name :", 0)
+pdf.cell(0, 10, renter_name, 0, 1,"C")
+pdf.cell(50, 10, "Address :", 0)
+pdf.cell(0, 10, address, 0, 1,"C")
+
+pdf.cell(0, 10, "", 0, 1)
+
 pdf.cell(50, 10, "Room Number :", 0)
-pdf.cell(0, 10, room_number, 0, 1)
+pdf.cell(0, 10, room_number, 0, 1,"C")
 
-# Add the start date
-pdf.cell(50, 10, "Start Date :", 0)
-pdf.cell(0, 10, start_date, 0, 1)
+pdf.cell(0, 10, "", 0, 1)
 
-# Add the end date
-pdf.cell(50, 10, "End Date :", 0)
-pdf.cell(0, 10, end_date, 0, 1)
+pdf.cell(50, 10, "Rent Amount :", 0)
+pdf.cell(0, 10, f"${rent_amount}", 0, 1,"C")
+pdf.cell(50, 10, "Deposit Amount :", 0)
+pdf.cell(0, 10, f"${deposit_amount}", 0, 1,"C")
+pdf.cell(50, 10, "Total Amount :", 0)
+pdf.cell(0, 10, f"${total_amount}", 0, 1,"C")
 
-# Add the price per night
-pdf.cell(150, 10, "Price per Night :", 0)
-pdf.cell(0, 10, f"${price_per_night}", 0, 1)
+pdf.cell(0, 10, "", 0, 1)
 
-# Add the total price
-pdf.cell(150, 10, "Total Price :", 0)
-pdf.cell(0, 10, f"${total_price}", 0, 1)
+pdf.cell(0, 10, "", 0, 1)
 
-# Save the PDF
-pdf.output("test.pdf")
+pdf.cell(50, 10, "Admin Name :", 0)
+pdf.cell(0, 10, admin_name, 0, 1,"C")
+pdf.cell(50, 10, "Phone Number :", 0)
+pdf.cell(0, 10, phone_number, 0, 1,"C")
+
+
+pdf.set_font("Arial", size=8, style="I")
+
+pdf.cell(0, 20, "", 0, 1)
+
+pdf.cell(0, 10, "Thank you for your payment!", 0, 1, "C")
+pdf.cell(0, 10, "Please contact us if you have any questions.", 0, 1, "C")
+pdf.line(10, 135, 200, 135)
+
+pdf.set_fill_color(255, 255, 204)
+#pdf.rect(0,270,210,0)
+
+
+pdf.output("pdf_nut.pdf")
