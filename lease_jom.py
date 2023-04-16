59df22a77bc5e9bb3429ecc35f6d807797583c4e
JittawatPdsr <116266763+Jittawatpdsr@users.noreply.github.com>
2023-04-17 00:34:12 +0700

    FPDF Master

--- lease_jom.py
+++ lease_jom.py
@@ -1,21 +1,22 @@
 from fpdf import FPDF
 
 
-admin_name = "Jittawat Praditseree"
-renter_name = "Haris Kirdpakdee"
-start_date = "April 1, 2023"
-end_date = "March 31, 2024"
+admin_name = "Jittawat Praditseree" #data base
+renter_name = "Haris Kirdpakdee" #data base 
+start_date = "April 1, 2023" #data base 
+end_date = "March 31, 2024" #data base 
 rent_amount = 1000
 security_deposit = 500
-address = "123 Pachalam Khuen Bog"
-room_number = "101"
+address = "123 Pachalam Khuen Bog" #data base 
+room_number = "101" #data base 
 
 pdf = FPDF()
 pdf.add_page()
 
 pdf.set_font("Arial", size=22, style="B")
 
-pdf.cell(0, 30, "Riski Aparment Lease Contact", 0, 1, "C")
+pdf.cell(0, 20, "Riski Aparment Lease Contact", 0, 1, "C")
+pdf.line(10, 28, 200, 28)
 
 pdf.set_font("Arial", size=12)
 
@@ -60,6 +61,14 @@ pdf.cell(0, 10, "The Admin : "f"{admin_name}",0 , 1)
 
 pdf.cell(0, 10, "1. Premises. The Landlord leases to the Renter the premises located at", 0, 1)
 pdf.cell(0, 10, "Address : "f"{address}",0, 1)
-pdf.cell(0, 10, "The Renter shall have exclusive use of the room numbered. "f"{room_number}", 0, 1)       
+pdf.cell(0, 10, "The Renter shall have exclusive use of the room numbered. "f"{room_number}", 0, 1)  
+
+def footer(self):
+        # Go to 1.5 cm from bottom
+    self.set_y(-15)
+        # Select Arial italic 8
+    self.set_font('Arial', 'I', 8)
+        # Print centered page number
+    self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
 
 pdf.output("lease_jom.pdf")
--- pdf_jom.py
+++ pdf_jom.py
@@ -1,14 +1,15 @@
 from fpdf import FPDF
+from PIL import Image
 
-renter_name = "Haris Kirdpakdee"
-room_number = "101"
+renter_name = "Haris Kirdpakdee" #data base
+room_number = "101" #data base
 rent_amount = 1000
 deposit_amount = 500
 total_amount = rent_amount + deposit_amount
-date = "April 16, 2023"
-address = "123 Pachalam Khuen Bog"
-admin_name = "Jittawat Praditseree"
-phone_number = "099-182-8589"
+date = "April 16, 2023" #data base
+address = "123 Pachalam Khuen Bog" #data base
+admin_name = "Jittawat Praditseree" #data base
+phone_number = "099-182-8589" #data base
 email = "jittawatofficial@gmail.com"
 
 pdf = FPDF()
@@ -16,41 +17,42 @@ pdf.add_page()
 
 pdf.set_font("Arial", size=22, style="B")
 
-pdf.cell(0, 30, "Riski Apartment Receipt", 0, 1, "C")
+pdf.cell(0, 15, "RISKI APARTMENT RECEIPT", 0, 1, "C")
+pdf.cell(0, 10, "", 0, 1)
+pdf.line(10, 28, 200, 28)
 
-pdf.set_font("Arial", size=12)
+pdf.set_font("Arial", size=12, style="B")
 
-pdf.cell(50, 10, "Date:", 0)
+pdf.cell(50, 10, "Date :", 0 )
 pdf.cell(0, 10, date, 0, 1,"C")
-pdf.cell(50, 10, "Renter Name:", 0)
+pdf.cell(50, 10, "Renter Name :", 0)
 pdf.cell(0, 10, renter_name, 0, 1,"C")
-pdf.cell(50, 10, "Address:", 0)
+pdf.cell(50, 10, "Address :", 0)
 pdf.cell(0, 10, address, 0, 1,"C")
 
 pdf.cell(0, 10, "", 0, 1)
 
-pdf.cell(50, 10, "Room Number:", 0)
+pdf.cell(50, 10, "Room Number :", 0)
 pdf.cell(0, 10, room_number, 0, 1,"C")
 
 pdf.cell(0, 10, "", 0, 1)
 
-pdf.cell(50, 10, "Rent Amount:", 0)
+pdf.cell(50, 10, "Rent Amount :", 0)
 pdf.cell(0, 10, f"${rent_amount}", 0, 1,"C")
-pdf.cell(50, 10, "Deposit Amount:", 0)
+pdf.cell(50, 10, "Deposit Amount :", 0)
 pdf.cell(0, 10, f"${deposit_amount}", 0, 1,"C")
-pdf.cell(50, 10, "Total Amount:", 0)
+pdf.cell(50, 10, "Total Amount :", 0)
 pdf.cell(0, 10, f"${total_amount}", 0, 1,"C")
 
 pdf.cell(0, 10, "", 0, 1)
 
 pdf.cell(0, 10, "", 0, 1)
 
-pdf.cell(50, 10, "Admin Name:", 0)
+pdf.cell(50, 10, "Admin Name :", 0)
 pdf.cell(0, 10, admin_name, 0, 1,"C")
-pdf.cell(50, 10, "Phone Number:", 0)
+pdf.cell(50, 10, "Phone Number :", 0)
 pdf.cell(0, 10, phone_number, 0, 1,"C")
-pdf.cell(50, 10, "Email:", 0)
-pdf.cell(0, 10, email, 0, 1,"C")
+
 
 pdf.set_font("Arial", size=8, style="I")
 
@@ -58,7 +60,10 @@ pdf.cell(0, 20, "", 0, 1)
 
 pdf.cell(0, 10, "Thank you for your payment!", 0, 1, "C")
 pdf.cell(0, 10, "Please contact us if you have any questions.", 0, 1, "C")
+pdf.line(10, 135, 200, 135)
 
 pdf.set_fill_color(255, 255, 204)
+#pdf.rect(0,270,210,0)
+
 
 pdf.output("pdf_jom.pdf")
