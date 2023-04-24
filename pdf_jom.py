59d17fad82e665d6774a6759ccc35c2ee46ac926
JittawatPdsr <116266763+Jittawatpdsr@users.noreply.github.com>
2023-04-24 17:23:36 +0700

    commit โง่ๆ

--- pdf_jom.py
+++ pdf_jom.py
@@ -8,23 +8,24 @@ def createconnection() :
 
 def payreceipt_pdf():
     
-    renter_name = "Haris Kirdpakdee" #data base customer
+    pdf = FPDF()
+    pdf.add_page()
+
+    renter_name = "จอม" #data base customer
     room_number = "101" #data base customer & room
     rent_amount = 1000
     deposit_amount = 500
     total_amount = rent_amount + deposit_amount
-    date = "April 16, 2023" #data base room
-    address = "123 Pachalam Khuen Bog" #data base 
-    admin_name = "Jittawat Praditseree" #data base user
+    date = "16/04/2023" #data base room
+    address = "123 ถ.นานานานานานานา" #data base 
+    admin_name = "จอม" #data base user
     phone_number = "099-182-8589" #data base user
     phone_number_rent = "081-817-8555"
+    
+    pdf.add_font('TH Sarabun New','',fname='THSarabunNew.ttf', uni=True)
+    pdf.add_font('TH Sarabun New Bold','B',fname='THSarabunNew Bold.ttf', uni=True)
 
-    pdf = FPDF()
-    pdf.add_page()
-    
-    pdf.add_font('angsana','','angsana.ttf',uni=True)
-    
-    pdf.set_font("angsana", size=22, style="B")
+    pdf.set_font("TH Sarabun New Bold", size=30 , style='B')
     pdf.encoding = 'utf-8'
     pdf.cell(0, 15, "RISKI APARTMENT RECEIPT", 0, 1, "C")
     pdf.cell(0, 10, "", 0, 1)
@@ -33,37 +34,38 @@ def payreceipt_pdf():
 
     pdf.cell(0, 10, "", 0, 1) #space
     
-    pdf.set_font("Arial", size=12, style="B")
-    pdf.cell(50, 10, "Date :", 0 )
-    pdf.cell(0, 10, date, 0, 1,"C")
-    pdf.cell(50, 10, "Renter Name :", 0)
+    pdf.set_font("TH Sarabun New", size=20 , style='')
+    pdf.cell(50, 10, "วันที่ :", 0 )
+    pdf.cell(0, 10, date, 0, 1 ,"C")
+    pdf.cell(50, 10, "ผู้เช่า :", 0)
     pdf.cell(0, 10, renter_name, 0, 1,"C")
-    pdf.cell(50, 10, "Phone Number :", 0)
+    pdf.cell(50, 10, "เบอร์โทรศัพท์ :", 0)
     pdf.cell(0, 10, phone_number_rent, 0, 1,"C")
-
+    
     pdf.cell(0, 10, "", 0, 1) #space
 
-    pdf.cell(50, 10, "Room Number :", 0)
+    pdf.cell(50, 10, "หมายเลขห้อง :", 0)
     pdf.cell(0, 10, room_number, 0, 1,"C")
 
     pdf.cell(0, 10, "", 0, 1) #space
 
-    pdf.cell(50, 10, "Rent Amount :", 0)
+    pdf.cell(50, 10, "ค่าเช่า :", 0)
     pdf.cell(0, 10, f"${rent_amount}", 0, 1,"C")
-    pdf.cell(50, 10, "Deposit Amount :", 0)
+    pdf.cell(50, 10, "ค่าน้ำ/ค่าไฟ :", 0)
     pdf.cell(0, 10, f"${deposit_amount}", 0, 1,"C")
-    pdf.cell(50, 10, "Total Amount :", 0)
+    pdf.cell(50, 10, "รวมทั้งหมด :", 0)
     pdf.cell(0, 10, f"${total_amount}", 0, 1,"C")
 
     pdf.cell(0, 10, "", 0, 1) #space
 
     pdf.cell(0, 10, "", 0, 1) #space
 
-    pdf.cell(50, 10, "Admin Name :", 0)
+    pdf.set_font("TH Sarabun New", size=20 , style='')
+    pdf.cell(50, 10, "เจ้าหน้าที่ :", 0)
     pdf.cell(0, 10, admin_name, 0, 1,"C")
-    pdf.cell(50, 10, "Phone Number :", 0)
+    pdf.cell(50, 10, "เบอร์โทรศัพท์ :", 0)
     pdf.cell(0, 10, phone_number, 0, 1,"C")
-    pdf.cell(50, 10, "Address :", 0)
+    pdf.cell(50, 10, "ที่อยู่ :", 0)
     pdf.cell(0, 10, address, 0, 1,"C")
 
 
